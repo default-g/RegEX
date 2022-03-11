0a7aeac4fb545b20d02e2dd00548233985c6a51c
Artem <arteam_101@mail.ru>
2022-03-11 10:47:57 +0700

    Добавьте файлы проекта.

---
+++ RegEX/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RegEX.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
