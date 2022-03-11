aba8cb262b7f95a9d8a188c2777408bd02b32b82
Artem <arteam_101@mail.ru>
2022-03-11 11:45:22 +0700

    Few changes

--- RegEX/ViewModels/MainWindowViewModel.cs
+++ RegEX/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,51 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ReactiveUI;
+using System.Text.RegularExpressions;
+
 
 namespace RegEX.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        string input = "";
+        string output = "";
+        string regex = "";
+        public MainWindowViewModel()
+        {
+       
+        }
+        public string Input
+        {
+            set
+            {
+                this.RaiseAndSetIfChanged(ref input, value);
+                Output = this.Input;
+            }
+            get
+            {
+                return this.input;
+            }
+        }
+        public string Output
+        {
+            set
+            {
+                this.RaiseAndSetIfChanged(ref output, value);
+            }
+            get
+            {
+                return this.output;
+            }
+        }
+        public void ChangeOutput()
+        {
+            this.output = this.input;
+
+
+        }
+    
     }
+ 
 }
